648e39e7097707b473fdaf91a0ec54477e7cf44c
T-Rekt <t.to.the.rekt@gmail.com>
2017-05-10 14:55:15 +0700

    bug fix

--- app.js
+++ app.js
@@ -14,29 +14,37 @@ app.use(bodyParser.text({type: '*/*'}));
 
 app.post("/", (req, res) => {
 	try {
-		console.log("New connection on "+Date.now());
-		request({
-			url: 'https://www.facebook.com/react_composer/feedx_sprouts/bootload/?composer_id=rc.u_0_r&target_id=1&composer_type=feedx_sprouts&friend_list_id=&dpr=1',
-			method: 'POST',
-			headers: {
-				'User-Agent': req.headers['user-agent'],
-				'Cookie': req.headers['cookie']
-			},
-			body: req.body,
-			strictSSL: false,
-		}, (err, resp, body) => {
-			if (err) {
-				res.send({});
-			}
-			else {
-				let json = JSON.parse(body.substr(9));
-				json.jsmods.require[0][3][1].config.taggersConfig.FORMATTED_TEXT.textFormats=preset;
-				res.send("for (;;);"+JSON.stringify(json));
-				// res.send(body);
-			}
-		});
+		if (req.body) {
+			console.log("New connection on "+Date.now());
+			request({
+				url: 'https://www.facebook.com/react_composer/feedx_sprouts/bootload/?composer_id=rc.u_0_r&target_id=1&composer_type=feedx_sprouts&friend_list_id=&dpr=1',
+				method: 'POST',
+				headers: {
+					'User-Agent': req.headers['user-agent'],
+					'Cookie': req.headers['cookie']
+				},
+				body: req.body,
+				strictSSL: false,
+			}, (err, resp, body) => {
+				if (err) {
+					res.send({});
+				}
+				else {
+					let json = JSON.parse(body.substr(9));
+					if (json.jsmods) {
+						json.jsmods.require[0][3][1].config.taggersConfig.FORMATTED_TEXT.textFormats=preset;
+						res.send("for (;;);"+JSON.stringify(json));
+					}
+					else {
+						res.send("Error");
+					}
+					// res.send(body);
+				}
+			});
+		}
 	} catch (e) {
 		console.log(e);
+		res.send("Error");
 	}
 });
 
